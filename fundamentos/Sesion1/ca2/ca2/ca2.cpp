4ef85866d956fc59a1c26d28673ce6f7e3d0247f
Cristofer Alberto Marin Astorga <107735454+TitoElTico@users.noreply.github.com>
2023-04-22 11:33:17 -0600

    Agregando la ca2

---
+++ fundamentos/Sesion1/ca2/ca2/ca2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    //Variable Declaration (type, identifier)
+    //int, platform dependant
+    //Value Initialize
+
+    int age{0};
+
+    std::string name{};
+
+    std::cout << "\nType your name: ";
+    std::cin >> name;
+
+    std::cout << "\nType your age: ";
+    std::cin >> age;
+
+    std::cout << "Hi " << name << " , your age is " << age <<<< std::end\;
+
+    
+}
